0ba0a46cc08897634acbfeeb89c784e8481d9085
arturisav <artur.isaverdyan@gmail.com>
2017-09-30 18:52:50 +0400

    test after test methods in same class

--- src/test/java/com/org/HelloW3SchoolTest.java
+++ src/test/java/com/org/HelloW3SchoolTest.java
@@ -6,23 +6,27 @@ import org.testng.annotations.Test;
 public class HelloW3SchoolTest extends BaseClass {
 
     @Test
-    public void login() throws InterruptedException {
+    public void goToBootstrap() throws InterruptedException {
 
-        System.out.println("=================== Hello.login test");
+        System.out.println("=================== go To Bootstrap Learning");
 //        System.setProperty("webdriver.gecko.driver", "C:\\devTools\\geckodriver.exe");
         String url = "http://www.w3schools.com/";
         driver.get(url);
         Thread.sleep(5000);
 //        go to bootstrap learning
         driver.findElement(By.xpath("//*[@id='mySidenav']/div/a[5]")).click();
-        Thread.sleep(5000);
 
 
     }
 
     @Test
-    public void logout(){
+    public void goToJquery() throws InterruptedException {
 
-        System.out.println("=================== Hello.logout test");
+        System.out.println("=================== go To Jquery Learning");
+//        String url = "http://www.w3schools.com/";
+//        driver.get(url);
+        Thread.sleep(5000);
+//        go to json learning
+        driver.findElement(By.xpath("//*[@id='topnav']/div/div[1]/a[9]")).click();
     }
 }
